1da67c15f580ec03171ca79445f3ab922b0e3f58
fld <xie.zhengqing@zhimatech.com>
2014-12-04 10:27:29 +0800

    add news admin

--- wecoders/news/admin.py
+++ wecoders/news/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from models import NewsSite
+from models import News, NewsSite
 
 class NewsSiteAdmin(admin.ModelAdmin):
 	pass
-	
+
+admin.site.register(News)
 admin.site.register(NewsSite, NewsSiteAdmin)
 
--- wecoders/news/models.py
+++ wecoders/news/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 
+class News(models.Model):
+    # id = models.IntegerField(default=0)
+    news_id = models.CharField(max_length=250, default=None)
+    site = models.CharField(max_length=200)
+    title = models.CharField(max_length=250)
+    sub_title = models.CharField(max_length=250)
+    url = models.CharField(max_length=250, default=None, blank=True)
+    source_url = models.CharField(max_length=250, default=None, blank=True)
+    sorts = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+    vote_count = models.IntegerField(default=0)
+    
+    class Meta:
+        db_table = "news"
+
+    def __unicode__(self):              # __unicode__ on Python 2
+        return "%d - %s - %s - %s" %(self.id, self.site , self.title, self.url)
+
 class NewsSite(models.Model):
     # id = models.IntegerField(default=0)
     uri = models.CharField(max_length=200)
